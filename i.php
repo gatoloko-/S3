c33dd1fa4492256e7e06208ebe236eb668cb5171
gatoloko- <ricomo@outlook.com>
2016-09-28 05:24:22 -0300

    start

---
+++ i.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" type="text/css" href="yadcf/jquery.dataTables.yadcf.css" />
+<link rel="stylesheet" type="text/css" href="ui/semantic.min.css" />
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/themes/smoothness/jquery-ui.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
+<script src="ui/semantic.js"></script>
---
+++ nav.php
@@ -0,0 +1,19 @@
+<div class="ui top attached menu">
+	<img src="img/logo_small.png" width="200" height="50">
+	<div class="item"></div>
+  <div class="ui dropdown item">
+    Ordenes
+    <div class="menu">
+      <div class="item">
+        <span class="text">Nueva</span>
+      </div>
+      <div class="item">
+        <span class="text">Editar</span>
+      </div>
+      <div class="item">
+        <span class="text">Eliminar</span>
+      </div>
+    </div>
+  </div>
+  
+</div>
