0cbc31af557f082973ee88d65ba2933a580fcf7e
gatoloko- <ricomo@outlook.com>
2016-09-28 13:40:41 -0300

    adicion movimientos
    movimientos, nav, edicion datatables

--- i.php
+++ i.php
@@ -5,5 +5,5 @@
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
 <script src="ui/semantic.js"></script>
-<script src="yadcf/jquery.dataTables.min.js"></script>
+<script src="yadcf/jquery.dataTables.js"></script>
 <script src="yadcf/jquery.dataTables.yadcf.js"></script>
--- nav.php
+++ nav.php
@@ -1,9 +1,14 @@
-<div class="ui top attached menu">
+<div class="ui top attached menu" id="menu">
 	<img src="img/logo_small.png" width="200" height="50">
 	<div class="item"></div>
   <div class="ui dropdown item">
     Ordenes
     <div class="menu">
+      <div class="item">
+      	<a href="home-2.php">
+      		<span class="text">Listado</span>
+      	</a>
+      </div>
       <div class="item">
         <span class="text">Nueva</span>
       </div>
@@ -16,4 +21,41 @@
     </div>
   </div>
   
+  <div class="ui dropdown item">
+    Movimientos
+    <div class="menu">
+      <div class="item">
+      	<a href="movimientos.php">
+      		<span class="text">Listado</span>
+      	</a>
+        
+      </div>
+      <div class="item">
+        <span class="text">Nuevo</span>
+      </div>
+      <div class="item">
+        <span class="text">Editar</span>
+      </div>
+      <div class="item">
+        <span class="text">Eliminar</span>
+      </div>
+    </div>
+  </div>
+  
 </div>
+<style>
+		#menu{
+			margin: 0px;
+		}
+		.ui.table td, th{
+			padding:3px;
+			
+		}
+		.paginate_button{
+			padding:3px;
+			cursor: pointer;
+		}
+		.dataTables_length{
+			padding:3px;
+		}
+	</style>
