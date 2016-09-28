895ce6ae232414343b7cd4be817598bbfbe3f068
gatoloko- <ricomo@outlook.com>
2016-09-28 07:45:49 -0300

    version home 2

--- home-2.php
+++ home-2.php
@@ -30,69 +30,48 @@
 	<table class="ui celled striped table" id="dash">
 		<thead>
 			<tr>
-				<th>Coordinador</th>
-				<th>Vendedor</th>
-				<th>Ingreso</th>
 				<th>ID</th>
 				<th>Cliente</th>
+				<th>Ingreso</th>
 				<th>AGA</th>
-				<th>Vehiculo</th>
-				<th>Conductor</th>
-				<th>Proveedor</th>
 				<th>Origen</th>
 				<th>Destino</th>
-				<th>Fecha recogida</th>
-				<th>Hora recogida</th>
-				<th>Servicio</th>
-				<th>Detalle</th>
-				<th>ID carga</th>
-				<th>Guia</th>
-				<th>Obs. Operaciones</th>
-				<th>Obs. Comercial</th>
+				<th>Recogida</th>
+				<th>Hora</th>
+				<th>Obs. Ope</th>
+				<th>Obs. Com</th>
+				<th>Coordinador</th>
+				<th>Vendedor</th>
 			</tr>
 		</thead>
 		<tbody>
 			<tr>
-				<td>jparra</td>
-				<td>Marcelino</td>
-				<td>29-09-2016</td>
 				<td>20x3</td>
 				<td>Transaco</td>
+				<td>29-09-2016</td>
 				<td>Aguirre</td>
-				<td>DPPG-69</td>
-				<td>JOSE FONSEA</td>
-				<td>TR3SVIAS</td>
 				<td>VALPARAISO</td>
 				<td>SANTIAGO</td>
 				<td>30-09-2016</td>
 				<td>15:30</td>
-				<td>FLETE</td>
-				<td>sin detalle</td>
-				<td>KKTU-598-3</td>
-				<td>256987</td>
 				<td></td>
 				<td></td>
+				<td>jparra</td>
+				<td>Marcelino</td>
 			</tr>
 			<tr>
-				<td>jobregon</td>
-				<td>Elizabeth</td>
-				<td>30-09-2016</td>
 				<td>40x2</td>
 				<td>Monsanto</td>
+				<td>30-09-2016</td>
 				<td>Aguirre</td>
-				<td>DPPG-69</td>
-				<td>JOSE FONSEA</td>
-				<td>TR3SVIAS</td>
 				<td>SAN ANTONIO</td>
 				<td>SANTIAGO</td>
 				<td>30-09-2016</td>
 				<td>15:30</td>
-				<td>FLETE</td>
-				<td>sin detalle</td>
-				<td>KKTU-598-3</td>
-				<td>256987</td>
 				<td></td>
 				<td></td>
+				<td>jobregon</td>
+				<td>Elizabeth</td>
 			</tr>
 		</tbody>
 	</table>
@@ -102,13 +81,14 @@
 $('.ui.dropdown').dropdown();
 $(document).ready(function(){
   $('#dash').dataTable().yadcf([
-		{column_number : 0,  column_data_type: "html", html_data_type: "text", filter_default_label: "Filtrar"},
 	    {column_number : 1,  column_data_type: "html", html_data_type: "text", filter_default_label: "Filtrar"},
 	    //{column_number : 3,  filter_type: "range_number_slider", filter_container_id: "external_filter_container"},
 	    //{column_number : 2, data: ["Yes", "No"], filter_default_label: "Select Yes/No"},
 	    //{column_number : 3, text_data_delimiter: ",", filter_type: "auto_complete"},
 	    {column_number : 4, column_data_type: "html", html_data_type: "text", filter_default_label: "Filtrar"},
-	    {column_number : 9,  column_data_type: "html", html_data_type: "text", filter_default_label: "Filtrar"}]);
+	    {column_number : 10, column_data_type: "html", html_data_type: "text", filter_default_label: "Filtrar"},
+	    {column_number : 11, column_data_type: "html", html_data_type: "text", filter_default_label: "Filtrar"}]);
+	    //{column_number : 9,  column_data_type: "html", html_data_type: "text", filter_default_label: "Filtrar"}
 });
 </script>
 	</body>
--- i.php
+++ i.php
@@ -1,7 +1,9 @@
-<link rel="stylesheet" type="text/css" href="yadcf/jquery.dataTables.yadcf.css" />
+
 <link rel="stylesheet" type="text/css" href="ui/semantic.min.css" />
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/themes/smoothness/jquery-ui.css">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
 <script src="ui/semantic.js"></script>
+<script src="yadcf/jquery.dataTables.min.js"></script>
+<script src="yadcf/jquery.dataTables.yadcf.js"></script>
